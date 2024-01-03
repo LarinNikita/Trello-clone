528630535d4e09de0f6ecfeed7b7bd2186dce777
LarinNikita <larin_nikita_m@mail.ru>
2024-01-03 21:18:38 +0700

    board start

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,15 +1,15 @@
+import { Separator } from "@/components/ui/separator";
+
+import { Info } from "./_components/info";
+
 const OrganizationIdPage = () => {
     return (
-        <div>
-            <form>
-                <input
-                    id="title"
-                    name="title"
-                    required
-                    placeholder="Enter a board title"
-                    className="border-input"
-                />
-            </form>
+        <div className="w-full mb-20">
+            <Info />
+            <Separator className="my-4" />
+            <div className="px-2 md:px-4">
+                {/* <BoardList /> */}
+            </div>
         </div>
     );
 };
