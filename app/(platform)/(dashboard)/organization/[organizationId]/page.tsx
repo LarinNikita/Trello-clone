3acbbf64207bb793e0f24cde61094dbec7f33d8a
LarinNikita <larin_nikita_m@mail.ru>
2024-01-04 12:22:24 +0700

    board popover form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@/components/ui/separator";
 
 import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
 
 const OrganizationIdPage = () => {
     return (
@@ -8,7 +9,7 @@ const OrganizationIdPage = () => {
             <Info />
             <Separator className="my-4" />
             <div className="px-2 md:px-4">
-                {/* <BoardList /> */}
+                <BoardList />
             </div>
         </div>
     );
