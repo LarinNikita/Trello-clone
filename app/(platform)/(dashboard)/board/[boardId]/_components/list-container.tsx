394ed2d377978ac22d2bbfa0c761d6af5b2add21
LarinNikita <larin_nikita_m@mail.ru>
2024-01-05 13:50:57 +0700

    prisma model list & card

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { ListWithCards } from "@/types";
+
+interface ListContainerProps {
+    bordId: string;
+    data: ListWithCards[];
+}
+
+export const ListContainer = ({
+    bordId,
+    data
+}: ListContainerProps) => {
+    return (
+        <div>
+            ListContainer
+        </div>
+    );
+};
--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,50 @@
-const BoardPageId = () => {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+    params: {
+        bordId: string;
+    };
+};
+
+const BoardPageId = async ({
+    params
+}: BoardIdPageProps) => {
+    const { orgId } = auth();
+
+    if (!orgId) {
+        redirect("/select-org");
+    }
+
+    const lists = await db.list.findMany({
+        where: {
+            boardId: params.bordId,
+            board: {
+                orgId,
+            },
+        },
+        include: {
+            cards: {
+                orderBy: {
+                    order: "asc"
+                },
+            },
+        },
+        orderBy: {
+            order: "asc",
+        },
+    });
+
     return (
-        <div>
-            Board ID
+        <div className="p-4 h-full overflow-x-auto">
+            <ListContainer
+                bordId={params.bordId}
+                data={lists}
+            />
         </div>
     );
 }
