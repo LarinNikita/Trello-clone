e6d7aaf8ce8bfa311b03facc0a3dbe8db666c986
LarinNikita <larin_nikita_m@mail.ru>
2024-01-07 20:46:22 +0700

    copy & delete card func

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -9,6 +9,7 @@ import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
 import { Header } from "./header";
 import { Description } from "./description";
+import { Actions } from "./actions";
 
 export const CardModal = () => {
     const id = useCardModal((state) => state.id);
@@ -39,6 +40,10 @@ export const CardModal = () => {
                             }
                         </div>
                     </div>
+                    {!cardData
+                        ? <Actions.Skeleton />
+                        : <Actions data={cardData} />
+                    }
                 </div>
             </DialogContent>
         </Dialog>
